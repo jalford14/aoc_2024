dcfac9e091d61431d7694c4de32746a26116b198
Jimmy Alford <jimmy.alford316@gmail.com>
2024-12-07 14:10:16 -0500

    day 5a finished

--- src/bin/day5a.rs
+++ src/bin/day5a.rs
@@ -1,10 +1,8 @@
 use std::collections::{HashMap, HashSet};
-use std::{thread, time};
 
 const INPUT: &str = include_str!("../../inputs/day5.txt");
 
 fn main() {
-    let time = time::Duration::from_millis(1000);
     let mut rules: HashMap<&str, Vec<&str>> = HashMap::new();
     let mut result = 0;
 
@@ -20,26 +18,23 @@ fn main() {
                         rules.entry(rules_to_insert[0]).and_modify(|val| val.push(rules_to_insert[1]));
                     }
                 } else if line.contains(",") {
-                    let page_numbers: Vec<&str> = line.split(",").collect();
+                    let mut page_numbers: Vec<&str> = line.split(",").collect();
                     let mut safe = true;
+                    page_numbers.reverse();
                     page_numbers
                         .iter()
                         .enumerate()
                         .for_each(|(i, page_number)| {
-                            thread::sleep(time);
+                            if !safe { return; }
                             if i < page_numbers.len() - 1 && rules.get(page_number) != None {
-                                println!("rules.get(page_number): {:?}", rules.get(page_number));
-                                for pgno_idx in (0..page_numbers.len()-2).rev() {
-                                    let rules_set: HashSet<&str> = HashSet::from(rules[page_number]);
-                                    let page_rules_to_check: HashSet<&str> = HashSet::from(page_numbers[0..pgno_idx]);
-                                    println!("pgno_idx {pgno_idx}");
-                                    println!("page_numbers[pgno_idx]: {}", page_numbers[pgno_idx]);
-                                    if rules_set.intersection(&page_rules_to_check).collect() == [] { safe = false; }
+                                let rules_set: HashSet<&str> = rules[page_number].iter().rev().cloned().collect();
+                                let page_rules_to_check: HashSet<&str> = page_numbers[i+1..].iter().rev().cloned().collect();
+                                if !rules_set.intersection(&page_rules_to_check).collect::<Vec<_>>().is_empty() { 
+                                    safe = false;
                                 }
                             }
                         });
                     if safe {
-                        println!("safe! {:?}", page_numbers);
                         result += page_numbers[page_numbers.len() / 2].parse::<i32>().unwrap();
                     }
                 }
